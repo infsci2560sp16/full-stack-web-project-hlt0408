e74cf103fa183dccf9f2e45a024608edfddd59fd
hlt0408 <huanglangtao08@gmail.com>
2016-02-12 22:09:38 -0500

    html dom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -63,6 +63,20 @@ public class Main {
 
         return new ModelAndView(attributes, "user-board.html");
     }, new FreeMarkerEngine());
+    
+    get("/purchase-history", (request, response) -> {
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("message", "Hello World!");
+
+        return new ModelAndView(attributes, "purchase-history.html");
+    }, new FreeMarkerEngine());
+    
+    get("/change-password", (request, response) -> {
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("message", "Hello World!");
+
+        return new ModelAndView(attributes, "change-password.html");
+    }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
       Connection connection = null;
